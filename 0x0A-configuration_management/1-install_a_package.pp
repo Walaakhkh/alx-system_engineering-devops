73a127a19df659e1eb5f5a86f71de905bbefa2e8
Walaakhkh <Walaakhalil98@gmail.com>
2024-06-28 00:15:48 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
-# install specific version of flask (2.1.0)
+# Install specific version of flask (2.1.0)
 package {'flask':
-	ensure		=> '2.1.0',
-	provider	=> 'pip'
+	ensure	 => '2.1.0',
+	provider => 'pip'
 }
+
