80bb07778805c2f421c40ce6e746d7ced3cc3bb8
Walaakhkh <Walaakhalil98@gmail.com>
2024-06-30 00:06:02 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# create a file
+# Creates a file
 file { '/tmp/school':
-	mode  	=> '0744',
-	owner  	=> 'www.data',
-	group  	=> 'www.data',
-	contant	=> 'I love Puppet'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
 
