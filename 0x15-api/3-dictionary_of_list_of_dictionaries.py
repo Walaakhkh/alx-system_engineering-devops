2a914e626a71083aff9b5412a77202fead059cb4
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-29 14:50:20 -0700

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 def fetch_user_data():
-    """Fetch user information and to-do lists for all employees. """
+    """Fetch user information and to-do lists for all employees."""
     url = "https://jsonplaceholder.typicode.com/"
 
     users = requests.get(url + "users").json()
@@ -14,20 +14,20 @@ def fetch_user_data():
     for user in users:
         user_id = user["id"]
 
-        todo_response = requests.get(url + f"todo?userId={user_id}")
-        
+        todo_response = requests.get(url + f"todos?userId={user_id}")
+
         todo_list = todo_response.json()
 
         data_to_export[user_id] = []
 
         for todo in todo_list:
             task_info = {
-                    "task": todo.get("title"),
-                    "completed": todo.get("completed"),
-                    "username": user.get("username")
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username")
             }
 
-            data_to_export[user_id]. append(task_info)
+            data_to_export[user_id].append(task_info)
 
     return data_to_export
 
@@ -36,3 +36,4 @@ if __name__ == "__main__":
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
+
