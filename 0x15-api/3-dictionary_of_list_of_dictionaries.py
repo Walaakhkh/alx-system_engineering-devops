7b2ae49fb2f1a78cabe92c2941cd2cb467a7494d
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-29 14:54:36 -0700

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 
+"""
+Module that fetches user information and to-do lists
+for all employees from a JSONPlaceholder API and
+exports the data to a JSON file.
+"""
+
 import json
 import requests
 
 def fetch_user_data():
     """Fetch user information and to-do lists for all employees."""
     url = "https://jsonplaceholder.typicode.com/"
-
     users = requests.get(url + "users").json()
-
     data_to_export = {}
 
     for user in users:
         user_id = user["id"]
-
         todo_response = requests.get(url + f"todos?userId={user_id}")
-
         todo_list = todo_response.json()
 
         data_to_export[user_id] = []
-
         for todo in todo_list:
             task_info = {
                 "task": todo.get("title"),
                 "completed": todo.get("completed"),
                 "username": user.get("username")
             }
-
             data_to_export[user_id].append(task_info)
 
     return data_to_export
@@ -36,4 +36,3 @@ if __name__ == "__main__":
 
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
-
