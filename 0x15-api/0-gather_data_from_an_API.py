624fe82eb6ba2fa2bcf8eb95fedac910e69d78c2
Walaaakhkh <Walaakhalil98@gmail.com>
2024-07-29 09:08:02 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python3
+"""
+0-gather_data_from_an_API.py
+
+Fetches and displays the TODO list progress for a given employee
+using a REST API.
+"""
+
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
 
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
     employee_id = sys.argv[1]
-    
-    user_response = requests.get(url + "users/{}".format(employee_id))
 
-    user = user_response.jason()
+    user_response = requests.get(f"{url}users/{employee_id}")
 
-    params = {"userId": employee_id}
+    if user_response.status_code != 200:
+        print("User not found")
+        sys.exit(1)
 
-    todos_response = requests.get(url + "todos", params=params)
+    user = user_response.json()
 
-    todos = todos_response.jason{}
+    todos_response = requests.get(f"{url}todos", params={"userId": employee_id})
 
-    completed = []
+    if todos_response.status_code != 200:
+        print("Todos not found")
+        sys.exit(1)
 
-    for todo in todos:
-        if todo.get("completed") is True:
-            completed.append(todo.get("title"))
+    todos = todos_response.json()
 
-    print("Employee {} is done with tasks({}/{})".format(user.get("name").
-                                                        len(completed). len(todos)))
+    completed = [todo.get("title") for todo in todos if todo.get("completed")]
+
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed), len(todos)))
     for complete in completed:
-        print("/t {}".format(complete))
+        print("\t {}".format(complete))
+
---
+++ 0x15-api/README.md
@@ -0,0 +1,11 @@
+# API TODO List Progress Fetcher
+
+This project fetches and displays the TODO list progress for a given employee using a REST API.
+
+## Usage
+
+To use the script, run the following command:
+
+```bash
+./0-gather_data_from_an_API.py <employee_id>
+
