f6e3b4c381d7db772438809c32e674c083b4d148
Walaaakhkh <Walaakhalil98@gmail.com>
2024-08-22 11:05:17 -0700

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,31 +1,17 @@
-# Puppet manifest to optimize Nginx server performance
+# Increases the amount of traffic on Nginx server can handle
 
-# Increase Nginx worker processes and connections
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_connections.*/worker_connections 4096;/" /etc/nginx/nginx.conf &&
-              sed -i "s/worker_processes.*/worker_processes auto;/" /etc/nginx/nginx.conf &&
-              systemctl restart nginx',
-  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+# Increase the ULIMIT of the defult file
+exec {  'fix--for-nginx':
+# Modify the ULIMIT value
+command => '/bin/sed -i "s/15/4096/" /etc/defult/nginx'
+# Specify the path for the sed command
+path	=> 'usr/local/bin/:/bin/',
 }
 
-# Ensure system limits are set to handle more connections
-file { '/etc/security/limits.conf':
-  ensure  => present,
-  content => @("EOF")
-*               soft    nofile          65535
-*               hard    nofile          65535
-  | EOF
-}
-
-# Increase the maximum number of open file descriptors
-exec { 'increase-file-descriptors':
-  command => 'echo "fs.file-max = 65535" >> /etc/sysctl.conf && sysctl -p',
-  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-}
-
-# Ensure Nginx service is running after changes
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Exec['fix--for-nginx'],
+# Restart Nginx
+exec {  'nginx-restart':
+# Restart Nginx services
+command => '/etc/inti.d/nginx restart',
+# Specify the path for the init.d script
+path	=> '/etc/init.d/',
 }
