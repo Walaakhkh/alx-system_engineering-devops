011682d3b1953c3888c89133227ca8edc7003581
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-24 01:40:32 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'custom:subscribers_count:v1.0 (by /u/yourusername)'}
+    headers = {
+'User-Agent': 'custom:subscribers_count:v1.0 (by /u/yourusername)'
+}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
