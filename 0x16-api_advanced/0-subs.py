b09e49d758d52a40cc2d5021bbcde237df7ce780
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-24 01:39:17 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+Module to query the Reddit API and return the number of subscribers
+for a given subreddit. If the subreddit is invalid, it returns 0.
+"""
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'custom:subscribers_count:v1.0 (by /u/yourusername)'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
