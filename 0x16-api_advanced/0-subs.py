a88460cfd25944f553e25a87bd460cde946005a5
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-24 02:22:13 -0700

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""
-Module to query the Reddit API and return the number of subscribers
-for a given subreddit. If the subreddit is invalid, it returns 0.
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-'User-Agent': 'custom:subscribers_count:v1.0 (by /u/yourusername)'
-}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            if "data" in data and "subscribers" in data["data"]:
-                return "OK"
-            else:
-                return "OK"
-        else:
-            return "OK"
-    except requests.RequestException:
-        return "OK"
+        "User-Agent": "Walaakhkh"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
