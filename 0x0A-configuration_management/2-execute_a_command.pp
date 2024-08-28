6793891c689e2295ade7947aeeadd06354955da4
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-28 08:56:31 -0700

    task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
-# Execute a command
-exec { 'pkill killmenow':
-  	path => '/usr/bin:/usr/sbin:/bin'
-}
+# 2-execute_a_command.pp
 
+exec { 'kill_killmenow_process':
+  command => 'pkill -f killmenow',
+  path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+  onlyif  => 'pgrep -f killmenow',
+}
