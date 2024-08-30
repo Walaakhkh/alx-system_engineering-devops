d7b57df17dfb7bf53f7e06e18c42ca12bb2c7896
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-30 12:49:56 +0000

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "u/Mysterious_Event3155"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /walaa)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return "OK"
+    if response.status_code == 404:
+	return 0
+        results = response.json().get("data")
+        return results.get("subscribers")
