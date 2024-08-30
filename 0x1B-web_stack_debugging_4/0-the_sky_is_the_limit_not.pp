8721f81bd0e5ffbe797c73332d103ef1ebc0cdd2
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-30 13:32:43 +0000

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,3 @@
-# Increases the amount of traffic on Nginx server can handle
-
-# Increase the ULIMIT of the defult file
-exec {  'fix--for-nginx':
-# Modify the ULIMIT value
-command => '/bin/sed -i "s/15/4096/" /etc/defult/nginx'
-# Specify the path for the sed command
-path	=> 'usr/local/bin/:/bin/',
-}
-
-# Restart Nginx
-exec {  'nginx-restart':
-# Restart Nginx services
-command => '/etc/inti.d/nginx restart',
-# Specify the path for the init.d script
-path	=> '/etc/init.d/',
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/worker_connections.*/worker_connections 1024;/g" /etc/nginx/nginx.conf && /usr/sbin/service nginx restart',
 }
