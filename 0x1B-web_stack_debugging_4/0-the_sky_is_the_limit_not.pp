1646f109148d23ca98a51dd520277dc4dfbf4016
Walaakhkh <Walaakhalil98@gmail.com>
2024-08-30 13:37:17 +0000

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,6 @@
+# testing how well our web server setup featuring Nginx is doing under pressure
+# and it turns out it is not doing well
 exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/worker_connections.*/worker_connections 1024;/g" /etc/nginx/nginx.conf && /usr/sbin/service nginx restart',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
 }
